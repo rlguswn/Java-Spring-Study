37aa6ecad5afbb772bd047ee16df0feca9e3712b
주기현 <0318joo@naver.com>
2024-07-19 12:56:16 +0900

    회원 서비스 생성 및 자바 코드로 스프링 빈 등록

---
+++ src/main/java/hello/hello_spring/Controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hello_spring.Controller;
+
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
