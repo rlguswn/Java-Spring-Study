6948ccdc8a50e3e4cb960f4256e676cc56805178
주기현 <0318joo@naver.com>
2025-05-29 16:59:59 +0900

    컨테이너에 등록된 모든 빈 조회

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -21,7 +21,7 @@ public class AppConfig {
     }
 
     @Bean
-    private MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
